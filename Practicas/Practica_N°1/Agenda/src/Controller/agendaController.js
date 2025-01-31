25502e9826c52d2c61aa8b31d20d4fc214ff27cd
ivespv <ivespv3@gmail.com>
2025-01-31 17:42:35 -0400

    apariencia

--- Practicas/Practica_N°1/Agenda/src/Controller/agendaController.js
+++ Practicas/Practica_N°1/Agenda/src/Controller/agendaController.js
@@ -27,9 +27,9 @@ async function crearAgenda(req, res) {
 
 // Actualizar un usuario
 const editarAgenda = async (req, res) => {
+    console.log("entro1")
     const { nombres, apellidos, direccion, telefono } = req.body;
     const EdAgenda = await getRepository(agenda).findOne(req.params.id);
-    console.log("entro1")
     if (EdAgenda) {
       EdAgenda.nombres = nombres;
       EdAgenda.apellidos = apellidos;
@@ -41,27 +41,6 @@ const editarAgenda = async (req, res) => {
       res.status(404).json({ mensaje: "Agenda no encontrada" });
     }
   };
-
-
-//async function editarAgenda(req, res) {
- // const { nombres, apellidos, direccion, telefono } = req.body;
- // try {
-  //    const agendaEntry = await getRepository(agenda).findOne(req.params.id);
-      //console.log (agendaEntry)
-  //    if (agendaEntry) {
-   //       agendaEntry.nombres = nombres;
-   //       agendaEntry.apellidos = apellidos;
-    //      agendaEntry.direccion = direccion;
-      //    agendaEntry.telefono = telefono;
-        //  const resultado = await getRepository(agenda).save(agendaEntry);
-          //res.json({ mensaje: "Agenda actualizada", data: resultado });
- //     } else {
-   //       res.status(404).json({ mensaje: "Registro no encontrado" });
-    //  }
-//  } catch (error) {
-  //    res.status(500).json({ mensaje: "Error al actualizar la agenda", error });
-//  }
-//};
 
 // Eliminar un agenda
 const eliminarAgenda = async (req, res) => {
