01ea12a39c5fba6bda4f8c3cd7f683e56ed3da6f
ivespv <ivespv3@gmail.com>
2025-02-14 10:12:10 -0400

    Guardar

--- Practicas/TrabajoFinal/CrudMongodb/app.js
+++ Practicas/TrabajoFinal/CrudMongodb/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const User = require('./models/User');
+const Cliente = require('./models/Cliente');
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 
 // Conexión a MongoDB
 // Conexión a MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/BdCliente', {
+mongoose.connect('mongodb://127.0.0.1:27017/RCliente', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
@@ -31,43 +31,49 @@ mongoose.connect('mongodb://127.0.0.1:27017/BdCliente', {
 app.get('/', async (req, res) => {
     const consulta = req.query.ci;
     if (consulta) {
-        const Cliente = await User.find({ ci: consulta} );
-        return res.render('index', { Cliente });
+        const clientes = await Cliente.find({ ci: consulta} );
+        return res.render('index', { clientes });
     }
-    const Cliente = await User.find( );
-    res.render('index', { Cliente });
+    const clientes = await Cliente.find( );
+    res.render('index', { clientes });
 });
 
 app.get('/Cliente/new', (req, res) => {
     res.render('create');
 });
 
+
 app.post('/Cliente', async (req, res) => {
-    const { ci, nombres, apellidos, celular, correo } = req.body;
-    await User.create({ ci, nombres, apellidos, celular, correo });
-    res.redirect('/');
+    try {
+        const { ci, nombres, apellidos, celular, correo } = req.body;
+        await Cliente.create({ ci, nombres, apellidos, celular, correo });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al crear el cliente');
+    }
 });
 
 app.get('/Cliente/:id', async (req, res) => {
-    const user = await User.findById(req.params.id);
-    res.render('show', { user });
-    console.log(user)
+    const cliente = await Cliente.findById(req.params.id);
+    res.render('show', { cliente });
+    console.log(cliente)
 });
 
 //////////////////////////////
 app.get('/Cliente/:id/edit', async (req, res) => {
-    const user = await User.findById(req.params.id);
-    res.render('edit', { user });
+    const cliente = await Cliente.findById(req.params.id);
+    res.render('edit', { cliente });
 });
 
 app.put('/Cliente/:id', async (req, res) => {
     const { ci, nombres, apellidos, celular, correo } = req.body;
-    await User.findByIdAndUpdate(req.params.id, { ci, nombres, apellidos, celular, correo});
+    await Cliente.findByIdAndUpdate(req.params.id, { ci, nombres, apellidos, celular, correo});
     res.redirect('/');
 });
 
 app.delete('/Cliente/:id', async (req, res) => {
-    await User.findByIdAndDelete(req.params.id);
+    await Cliente.findByIdAndDelete(req.params.id);
     res.redirect('/');
 });
 
--- Practicas/TrabajoFinal/CrudMongodb/models/Cliente.js
+++ Practicas/TrabajoFinal/CrudMongodb/models/Cliente.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const clienteSchema = new mongoose.Schema({
     ci: { type: Number, required: true },
     nombres: { type: String, required: true },
     apellidos: { type: String, required: true },
     celular: { type: Number, required: true },
-    correo: { type: String, required: true },
+    correo: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Cliente', clienteSchema);
