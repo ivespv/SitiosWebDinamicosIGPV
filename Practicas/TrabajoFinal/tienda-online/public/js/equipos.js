4ba46cc7e08b4ba3a64ce597ec60ce6cf25f9ce6
ivespv <ivespv3@gmail.com>
2025-02-16 17:15:05 -0400

    tienda-online

---
+++ Practicas/TrabajoFinal/tienda-online/public/js/equipos.js
@@ -0,0 +1,52 @@
+/* Esta lista podría ser reemplazada por la respuesta de un backend */
+const equipos = [
+  {
+    id:1,
+    nombre: "CENTRAL TELEFONICA CISCO C881-V-K9 ",
+    precio: 3000,
+    detalle: "El enrutador de servicios integrados Cisco 881 provee una solución colaborativa de negocios segura para la comunicación de telefonía y de datos. Este enrutador es ideal para pequeños negocios,sucursales y teleworkers. El enrutador puede ser utilizado como una central telefónica IP con soporte de 5 teléfonos IP o puede funcionar como un gateway de Supervivencia Remota con la solución Cisco Unified Communications Manager (CUCM)."
+  },
+  {
+    id:2,
+    nombre: "FIREWALL FORTINET FortiGate-40F",
+    precio: 1500,
+    detalle: "Sobre este artículo\n" +
+            "Diseño compacto y eficiente: el FortiGate 40F está diseñado para pequeñas y medianas empresas y sucursales empresariales, con un factor de forma de escritorio compacto y sin ventilador que garantiza un funcionamiento silencioso y minimiza el uso del espacio.\n\n" +
+            "Opciones de conectividad robustas: Equipado con 5 puertos GE RJ45, incluyendo 1 puerto WAN y 4 puertos internos, este modelo proporciona conectividad y flexibilidad esenciales para varias configuraciones de red en un entorno de pequeña escala.\n\n" +
+            "Seguridad de alto rendimiento: ofrece un rendimiento IPS de hasta 1 Gbps y un rendimiento de protección contra amenazas de 600 Mbps, utilizando la tecnología de procesador de seguridad especialmente diseñada para ofrecer rendimiento y protección líderes en la industria para el tráfico cifrado SSL.\n\n" +
+            "Protección avanzada contra amenazas: Integrado con los laboratorios FortiGuard basados en IA de Fortinet, el FortiGate 40F ofrece ciberseguridad integral, identificando y mitigando amenazas conocidas y desconocidas para mantener una seguridad sólida en toda su red.\n\n" +
+            "Administración e implementación simplificadas: Cuenta con una consola de administración fácil de usar que proporciona una automatización y visibilidad de red integrales, junto con la integración Zero Touch con Security Fabric de Fortinet para una fácil implementación."
+  },
+  {
+    id:3,
+    nombre: "Router de servicios integrados Cisco 4431",
+    precio: 7000,
+    detalle: "Un dispositivo para satisfacer las necesidades de su sucursal\n"+
+            "Para desarrollar las capacidades digitales en las sucursales de su empresa, los enrutadores de servicios integrados de la serie Cisco ® 4000 (ISR 4000) han sido la opción de plataforma más obvia desde 2014. La serie ISR 4000 consolida muchas funciones de TI imprescindibles, incluidas la red, la seguridad, la computación, el almacenamiento y las comunicaciones unificadas. Obtiene todo lo que necesita en una única plataforma. Eso significa ahorros significativos en gastos de capital, operativos y de administración para un menor costo total de propiedad.\n\n" +
+            "La plataforma es modular y actualizable, por lo que puede agregar nuevos servicios sin cambiar el equipo. Admite múltiples servicios que reconocen las aplicaciones simultáneamente mientras mantiene el rendimiento de la WAN de hasta 7 Gbps (ISR4461), incluso durante cargas de tráfico pesadas. La arquitectura de la placa base admite comunicaciones de módulo a módulo de gran ancho de banda a velocidades de hasta 10 Gbps. La serie ISR 4000 incluye tecnologías Cisco Trust Anchor que ayudan a mitigar los ciberataques modernos al verificar la integridad de la\n\n" +
+            "plataforma y brindar protección contra la falsificación y la modificación no autorizada de hardware y software. La serie 4000 original contiene nueve plataformas: ISR 4461, 4451, 4431, 4351, 4331, 4331-DC, 4321, 4221 y 4221X."
+  },
+  {
+    id:4,
+    nombre: "Cisco Switch Catalyst 9300",
+    precio: 4000,
+    detalle: "Brinde una experiencia más segura y sostenible que combine velocidad y escalabilidad con análisis y automatización avanzados.\n"+
+            "Transforme su lugar de trabajo - Obtenga el ancho de banda, la velocidad y la escalabilidad que necesita para respaldar a los equipos híbridos desde donde trabajen.\n\n" +
+            "Proteja su red de adentro hacia afuera - Potencie su red con cifrado integrado y AI/ML avanzados para una seguridad de confianza cero continua.\n\n" +
+            "Administre su red a su manera - Ofrezca experiencias de red excepcionales con la plataforma Cisco Catalyst Center o el monitoreo en la nube para Catalyst.\n\n" +
+            "Genere sostenibilidad en su red - Use la red como base para edificios más sostenibles e inteligentes con un rendimiento energético óptimo, que promuevan el bienestar de sus ocupantes."
+  },
+  {
+    id:5,
+    nombre: "Cámaras de seguridad Dahua",
+    precio: 1200,
+    detalle: "Kit cámaras de seguridad 1 Megapixel - En la actualidad los circuitos cerrados de televisión o CCTV son parte fundamental de nuestra seguridad. Es una tecnología diseñada para vigilar y supervisar ambientes cerrados como nuestra vivienda, nuestros negocios y nuestras oficinas; y también para vigilar ambientes abiertos como una calle o un patio. En un circuito cerrado de televisión se encuentran instalados gran variedad de componentes conectados entre sí; que crean a su vez un circuito de imágenes que no puede ser visto por otra persona fuera de él. Por eso te invitamos a conocer el nuevo Kit cámaras de seguridad 1 Megapixel\n"+
+            "Los circuitos cerrados de televisión pueden ser diseñados para adaptarse en cualquier escenario y a cualquier necesidad cuando lo quieras y como lo quieras. En casa, oficina o empresa funcionan a la perfección, no te quedes sin el tuyo."
+  },
+  {
+    id:6,
+    nombre: "CicloLoco",
+    precio: 150,
+    detalle: "Sistemas telefónicos para pequeñas empresas - La tecnología de voz por IP (VoIP) moderniza el sistema telefónico a fin de poder usar Internet para hacer y recibir llamadas de teléfono fijo y teléfono celular o de computadora a computadora."
+  }
+]
