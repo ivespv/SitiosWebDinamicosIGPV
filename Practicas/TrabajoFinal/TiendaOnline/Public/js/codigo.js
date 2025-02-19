893351c7810bc40ac35c9be0a13c1854b7233cea
ivespv <ivespv3@gmail.com>
2025-02-19 14:09:45 -0400

    V1.1

--- Practicas/TrabajoFinal/TiendaOnline/Public/js/codigo.js
+++ Practicas/TrabajoFinal/TiendaOnline/Public/js/codigo.js
@@ -1,33 +1,3 @@
-const baseDeDatos = [
-    {
-        id: 1,
-        nombre: 'FORTIGATE 100F',
-        precio: 15000,
-        imagen: 'Imagenes/FortiGate 100F.jpg',
-        imagen1: 'Imagenes/fotigate.webp'
-    },
-    {
-        id: 2,
-        nombre: 'Router Cisco 4451-X',
-        precio: 49000,
-        imagen: 'Imagenes/R1.jpg',
-        imagen1: 'Imagenes/R2.jpg',
-    },
-    {
-        id: 3,
-        nombre: 'Switches Cisco 9300',
-        precio: 27000,
-        imagen: 'Imagenes/s1.jpg',
-        imagen: 'Imagenes/s2.jpg',
-    },
-    {
-        id: 4,
-        nombre: 'Central Telefonica IP CISCO C2921-CME-SRST',
-        precio: 25000,
-        imagen: 'Imagenes/C1.png',
-        imagen: 'Imagenes/C2.png',
-    }
-];
 let carrito = [];
 const divisa = '€';
 const DOMitems = document.querySelector('#items');
--- Practicas/TrabajoFinal/TiendaOnline/app.js
+++ Practicas/TrabajoFinal/TiendaOnline/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./db'); // Asegúrate de que tu archivo db.js esté configurado correctamente
+const db = require('./db'); 
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));  // Para poder leer los datos de un formulario
+app.use(bodyParser.urlencoded({ extended: true }));  
 
 app.set('view engine', 'ejs'); // Configurar EJS como motor de plantillas
 app.use(express.static('public')); // Servir archivos estáticos
